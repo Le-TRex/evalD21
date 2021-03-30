411aa615f0307b028ed915ca6bae902681ed6665
Le-TRex <clarembo@gmail.com>
2021-03-30 21:22:46 +0200

    :construction: WIP Question component works but toggle type questions must be modified using material-ui

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -6,25 +6,36 @@ function Question ({question}) {
   function displayQuestionDependingOnItsType(question){
     const type = question.answer.type
     const options = question.answer.options
+    const answerName = question.answer.name
 
     if (type === "radio") {
-
-
       return (
         <Fragment>
           <p>{question.question}</p>
           <div>
             {options.map((option, index) => (
-              <p key={index}>{option.text}</p>
+              <div key={index}>
+                <input type={type} name={answerName} value={option.value} id={option.value}/>
+                <label htmlFor={option.value}>{option.text}</label>
+              </div>
             ))}
           </div>
-
         </Fragment>
       )
-
     }else if (type === "checkbox"){
-
-
+      return(
+        <Fragment>
+          <p>{question.question}</p>
+          <div>
+            {options.map((option, index) => (
+              <div key={index}>
+                <input type={type} name={option.name} id={option.name}/>
+                <label htmlFor={option.name}>{option.text}</label>
+              </div>
+            ))}
+          </div>
+        </Fragment>
+      )
     }else if (type === "text"){
       return (
         <p>
@@ -33,7 +44,18 @@ function Question ({question}) {
         </p>
       )
 
-    }else{
+    }else {
+      return (
+        <Fragment>
+          <p>{question.question}</p>
+          <div>
+            <input type="radio" name={answerName} value="oui" id="oui"/>
+            <label htmlFor="oui">oui</label>
+            <input type="radio" name={answerName} value="non" id="non"/>
+            <label htmlFor="non">non</label>
+          </div>
+        </Fragment>
+      )
     }
   }
 
