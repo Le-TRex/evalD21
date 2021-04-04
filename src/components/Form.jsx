ef28f72915e8e5fe6304efcd16f90ddfd4ed31dd
Le-TRex <clarembo@gmail.com>
2021-04-04 12:18:59 +0200

    :bulb: way to check if form values are properly caught

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,7 @@ import '../stylesheets/form.css'
 
 function Form(){
   return(
+    // <form action="#" method="GET"> pour tester la récupération des valeur
     <form action="http://188.165.104.163/exam" method="POST">
       {exam.map((question, index)=>(
         <Question question={question} key={index}/>
